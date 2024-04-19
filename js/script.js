bf8ff1b47754c5882c51ffee3451cbbbd467dd97
Yohanaaaaa <yohanaamorim789@gmail.com>
2024-04-19 19:07:08 -0300

    salvei tudo que tinha esquecido

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const mode = document.getElementById('modeIcon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('loginForm');
+    if(mode.classList.contains('fa-moon')){
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+        form.classList.add('dark');
+    }else { 
+        mode.classList.remove('fa-sun');
+        mode.classList.add('fa-moon');
+        form.classList.remove('dark');
+    } 
+})
